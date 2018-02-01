d3d840b00a4eda8d98f30bd9165a3a95aeb63df1
melania <melania.gallego.22@gmail.com>
2018-02-01 13:46:15 +0100

    u2_controles_ej20_CheckBox,RadioGroup,Spinners

--- app/src/main/java/com/example/didact/u2_controles_ej20_actividadceckboxyradiogroup/MainActivity.java
+++ app/src/main/java/com/example/didact/u2_controles_ej20_actividadceckboxyradiogroup/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     CheckBox cb_sopa, cb_ensalada, cb_nachos, cb_carne, cb_pescado;
     RadioGroup rg_sino;
-    Spinner sp_provincias;
+    Spinner sp_provincias, sp_bebidas;
 
 
     @Override
@@ -30,13 +30,23 @@ public class MainActivity extends AppCompatActivity {
         cb_pescado= (CheckBox)findViewById(R.id.cb_pescado);
         rg_sino= (RadioGroup)findViewById(R.id.rg_sino);
         sp_provincias= (Spinner)findViewById(R.id.sp_provincias);
+        sp_bebidas= (Spinner)findViewById(R.id.sp_bebidas);
 
+        //Spinner bebidas
+        String [] bebidas = {"Cocacola","Fanta","Sprite","Neztea","Zumo","Agua","Vino"};
+
+        ArrayAdapter<String> adaptador1 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_expandable_list_item_1,bebidas);
+
+        sp_bebidas.setAdapter(adaptador1);
+
+        //Spinner provincias
         String [] provincias = {"Cádiz", "Mñaga","Almeria","Cordoba"};
 
-        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this,
+        ArrayAdapter<String> adaptador2 = new ArrayAdapter<String>(this,
                 android.R.layout.simple_expandable_list_item_1,provincias);
 
-        sp_provincias.setAdapter(adaptador);
+        sp_provincias.setAdapter(adaptador2);
 
     } //FIN onCreate
 
@@ -67,8 +77,13 @@ public class MainActivity extends AppCompatActivity {
         RadioButton rbSeleccionado = (RadioButton)findViewById(id);
         String textoRbSeleccionado = rbSeleccionado.getText().toString();
 
-        Toast.makeText(getApplicationContext(), "Tu Pedido es: "+preferencias+textoRbSeleccionado+" bebida",
+        //String de spinner de bebidas
+        String bebidaSeleccionada = sp_bebidas.getSelectedItem().toString();
+
+        Toast.makeText(getApplicationContext(), "Tu Pedido es: "+preferencias+textoRbSeleccionado+" bebida seleccionada:"+bebidaSeleccionada,
                 Toast.LENGTH_SHORT).show();
+
+
 
 
 
@@ -79,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
 
         String provinciaSeleccionada = sp_provincias.getSelectedItem().toString();
         Toast.makeText(getApplicationContext(), "Proicnia "+provinciaSeleccionada,
-                Toast.LENGTH_SHORT).show();
+                Toast.LENGTH_LONG).show();
     }
 
 
